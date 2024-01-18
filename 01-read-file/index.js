dae54aca96c92d7a43a785a4df91c46ca2065787
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-01-18 10:06:35 +0300

    01-read-file: add encoding to readfile

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 console.log('Hello world!');
 filePath = path.resolve('01-read-file', 'text.txt');
 
-fs.readFile(filePath, (err, data) => {
+fs.readFile(filePath,'utf8', (err, data) => {
     console.log(filePath);
     console.log('читаю данные из файла');
     console.log(data);
