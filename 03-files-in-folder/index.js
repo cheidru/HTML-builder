ee5d5ff2647c026093585025a6d3fc87b63f4b13
Igor Checheurov <chei@yandex.ru>
2024-01-21 22:19:21 +0300

    fix: fix output of 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,14 +6,14 @@ const secretFolderPath = path.resolve('03-files-in-folder', 'secret-folder');
 // The callback of this method returns an array of all the file names in the directory.
 // OPTIONS recursive: true - includes subdirs; withFileType: true - specifies whether 
 // the files would be returned as fs.Dirent objects
-const filenames = fs.readdir(secretFolderPath, (err, files) => {
+fs.readdir(secretFolderPath, (err, files) => {
     for (const file of files) {
         const itemPath = path.resolve('03-files-in-folder', 'secret-folder', file);
         // if(fs.stat(path.basename(file))
         fs.stat(itemPath, (err, stats) => {
             if (stats.isFile()) {
                 const exten = path.extname(itemPath);
-                const outString = `${file.slice(0, -exten.length)} - ${exten} - ${(stats.size/1024).toFixed(3)}kb`;
+                const outString = `${file.slice(0, -exten.length)} - ${exten.slice(1)} - ${(stats.size/1024).toFixed(3)}kb`;
                 console.log(outString);
             }
         });
