08f26a67497be09fcaa6b499d92737acaae369a0
Igor Checheurov <chei@yandex.ru>
2024-01-21 23:14:51 +0300

    feat: complete 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.resolve('04-copy-directory');
+const fileDirPath = path.resolve('04-copy-directory', 'files');
+const fileCopyDirPath = path.resolve('04-copy-directory', 'files-copy');
+
+fs.readdir(dirPath, (err, items) => {
+    if(items.includes('files-copy')) {
+        fs.readdir(fileCopyDirPath, (err, files) => {
+            for(const file of files) {
+                const filePath = path.resolve(fileCopyDirPath, file);
+                fs.unlink(filePath, () => {});
+            }
+            fs.rmdir(fileCopyDirPath, () => {
+                copyFilesDir();
+            });
+
+        })        
+    } else {
+        copyFilesDir();
+    }    
+})
+
+function copyFilesDir() {
+    fs.mkdir(fileCopyDirPath, () => {});
+    fs.readdir(fileDirPath, (err, files) => {
+        for (const file of files) {
+            const filePath = path.resolve(fileDirPath, file);
+            const fileCopyPath = path.resolve(fileCopyDirPath, file);
+            fs.copyFile(filePath, fileCopyPath, () => {});
+        }
+    })
+}
