060d6e7b7cb3fa908dc7aa6b3fb2453f74bd3c3f
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-01-22 16:28:39 +0300

    feat: start 06-build-page task

---
+++ 06-build-page/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+// const dirPath = path.resolve('05-merge-styles');
+const componentDirPath = path.resolve('06-build-page', 'components');
+const distDirPath = path.resolve('06-build-page', 'project-dist');
+const distAssetsDirPath = path.resolve('06-build-page', 'project-dist', 'assets');
+const stylesDirPath = path.resolve('06-build-page', 'styles');
+const assetsDirPath = path.resolve('06-build-page', 'assets');
+const indexFilePath = path.resolve('06-build-page', 'project-dist', 'index.html');
