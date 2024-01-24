2983db56efcf4fe3ac9e7ec579bd56b9f60108ba
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-01-24 08:29:08 +0300

    fix: restore last version of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,51 +1,31 @@
 const fs = require('fs');
 const path = require('path');
-const cssmerge = require('../05-merge-styles/index.js');
-const assetcopy = require('../04-copy-directory/index.js');
 
-const componentDirPath = path.resolve('06-build-page', 'components');
-const distDirPath = path.resolve('06-build-page', 'project-dist');
-const indexFilePath = path.resolve('06-build-page', 'project-dist', 'index.html');
-const distAssetsDirPath = path.resolve('06-build-page', 'project-dist', 'assets');
-const distStylePath = path.resolve('06-build-page', 'project-dist', 'style.css');
-const stylesDirPath = path.resolve('06-build-page', 'styles');
-const assetsDirPath = path.resolve('06-build-page', 'assets');
-const templateFilePath = path.resolve('06-build-page', 'template.html');
-const articlesFilePath = path.resolve('06-build-page', 'components', 'articles.html');
-const footerFilePath = path.resolve('06-build-page', 'components', 'footer.html');
-const headerFilePath = path.resolve('06-build-page', 'components', 'header.html');
+const dirPath = path.resolve('04-copy-directory');
+const fileDirPath = path.resolve('04-copy-directory', 'files');
+const fileCopyDirPath = path.resolve('04-copy-directory', 'files-copy');
 
-let indexStr = '';
-let articlesStr = '';
-let footerStr = '';
-let headerStr = '';
 
-fs.mkdir(distDirPath, () => {});
+function dirCopy(distFolder, origFolder, copyFolder) {
+    fs.readdir(distFolder, (err, items) => {
+        if(items.includes(path.basename(copyFolder))) {
+            fs.readdir(copyFolder, (err, files) => {
+                for(const file of files) {
+                    const filePath = path.resolve(copyFolder, file);
+                    fs.unlink(filePath, () => {});
+                }
+                fs.rmdir(copyFolder, () => {});
+            })
+        }
+        fs.mkdir(copyFolder, () => {});
+        fs.readdir(origFolder, (err, files) => {
+            for (const file of files) {
+                const filePath = path.resolve(origFolder, file);
+                const fileCopyPath = path.resolve(copyFolder, file);
+                fs.copyFile(filePath, fileCopyPath, () => {});
+            }
+        })
+    })
+}
 
-const getTemplateContent = fs.createReadStream(templateFilePath, {encoding: 'utf8'});
-getTemplateContent.on('data', (data) => {
-    indexStr = data;
-});
-
-const getArticlesContent = fs.createReadStream(articlesFilePath, {encoding: 'utf8'});
-getArticlesContent.on('data', (data) => {
-    articlesStr = data;
-    indexStr = indexStr.replace('{{articles}}', articlesStr);
-});
-
-const getFooterContent = fs.createReadStream(footerFilePath, {encoding: 'utf8'});
-getFooterContent.on('data', (data) => {
-    footerStr = data;
-    indexStr = indexStr.replace('{{footer}}', footerStr);
-});
-
-const getHeaderContent = fs.createReadStream(headerFilePath, {encoding: 'utf8'});
-getHeaderContent.on('data', (data) => {
-    headerStr = data;
-    indexStr = indexStr.replace('{{header}}', headerStr);
-    const getIndexFile = fs.createWriteStream(indexFilePath, {encoding: "utf-8"});
-    getIndexFile.write(indexStr);
-});
-
-cssmerge.mergeStyles(distDirPath, distStylePath, stylesDirPath);
-assetcopy(distDirPath, assetsDirPath, distAssetsDirPath);
+dirCopy(dirPath, fileDirPath, fileCopyDirPath);
