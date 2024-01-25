7e0dd930d3d2e77af124bcf814aa7e99ffba335b
Igor Checheurov <chei@yandex.ru>
2024-01-25 22:07:05 +0300

    feat: complete task6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -13,69 +13,35 @@ const templateFilePath = path.resolve('06-build-page', 'template.html');
 
 let indexStr = '';
 let templateStr = '';
-let folders = [];
+let final = '';
 
 
 async function makeDistFolder() {
     fs.readdir(projectPath, (err, data) => {
         if(data.includes('project-dist')) {
-            folders = [];
             clearFolder(distDirPath);
         }
     });
 
-    // await fs.promises.mkdir(distDirPath, (err) => {
-    //     if (err) throw err;
-    // });
-    // copyAsset(assetsDirPath, distAssetsDirPath);
-    // makeHTML (componentDirPath);
-    // mergeStyles(distDirPath, distStylePath, stylesDirPath);
+    await fs.promises.mkdir(distDirPath, (err) => {
+        if (err) throw err;
+    });
+    await fs.promises.mkdir(distAssetsDirPath, (err) => {
+        if (err) throw err;
+    });
+    await copyAsset(assetsDirPath, distAssetsDirPath);
+    await makeHTML(componentDirPath);
+    await writeHTML();
+
+    mergeStyles(distDirPath, distStylePath, stylesDirPath);
 }
 
-function clearFolder(folderPath) {
-    console.log('---------------------удаляем папку---------------------', folderPath);
-    fs.readdir(folderPath, (err, files) => {
-    // fs.rmdir(folderPath, (err, files) => { 
-        console.log('Читаем папку ', folderPath);
-        // folders.push(folderPath);
-        if (err) {
-            console.log('Ошибка чтения папки ', folderPath);
-        } else {
-            console.log('Проходим по files ', files);
-            for(const file of files) {
-                console.log('Текущий file ', file);
-                const filePath = path.resolve(folderPath, file);            
-                fs.stat(filePath, (err, stats) => {
-                    if (stats.isFile()) {
-                        console.log('удаляем файл ', file);
-                        fs.unlink(filePath, () => {});
-                    } else {
-                        clearFolder(filePath);
-                    }
-                })
-            }
-            // console.log('удаляем папки из массива', folders);
-            // for(const fold of folders) {
-            //     console.log(fold);
-            //     fs.rmdir(folders.pop(), (e) => {
-            //         if (e) {
-            //             console.log('не могу удалить папку', fold);
-            //         }
-            //     })
-            // }
-        }
-        fs.rmdir(folderPath, (e) => {
-            console.log('Удаляем папку ', folderPath);
-            if (e) {
-                console.log('не могу удалить папку', folderPath);
-            }
-        })
-    })
-
+async function clearFolder(folderPath) {
+    await fs.promises.rm(folderPath, { force: true, recursive: true });
 }
 
-async function copyAsset(origFolder, copyFolder) {
-    await fs.promises.readdir(origFolder, (err, files) => {
+async function copyAsset(origFolder, copyFolder) {    
+    fs.readdir(origFolder, (err, files) => {
         for (const file of files) {
             const filePath = path.resolve(origFolder, file);
             fs.stat(filePath, (err, stats) => {
@@ -84,7 +50,7 @@ async function copyAsset(origFolder, copyFolder) {
                     fs.copyFile(filePath, fileCopyPath, () => {});
                 } else {
                     const folderCopyPath = path.resolve(copyFolder, file);
-                    fs.rmdir(folderCopyPath, () => {});
+                    fs.mkdir(folderCopyPath, () => {});
                     copyAsset(filePath, folderCopyPath);
                 }
             })
@@ -92,26 +58,23 @@ async function copyAsset(origFolder, copyFolder) {
     })
 }
 
-
-async function makeHTML (componFolder) {
-    console.log('Компонуем html');
-    await fs.promises.readdir(componFolder, (err, files) => {
-        const getTemplateContent = fs.createReadStream(templateFilePath, {encoding: 'utf8'});
-        getTemplateContent.on('data', (data) => {
-            indexStr = data;
+async function makeHTML(componFolder) {
+    const files = await fs.promises.readdir(componFolder);
+    const getTemplateContent = fs.createReadStream(templateFilePath, {encoding: 'utf8'});
+    getTemplateContent.on('data', (data) => {
+        indexStr = data;
+    });
+    for (const file of files) {
+        const filePath = path.resolve(componFolder, file);
+        const templateName = `${file.slice(0, -5)}`;
+        const templateSream = fs.createReadStream(filePath, {encoding: 'utf8'});
+        templateSream.on('data', (data) => {
+            templateStr = data;
+            indexStr = indexStr.replace(`{{${templateName}}}`, templateStr);
+            const getIndexFile = fs.createWriteStream(indexFilePath, {encoding: "utf-8"});
+            getIndexFile.write(indexStr);
         });
-        for (const file of files) {
-            const filePath = path.resolve(componFolder, file);
-            const templateName = `{{${file.slice(0, -3)}}}`;
-            const templateSream = fs.createReadStream(filePath, {encoding: 'utf8'});
-            templateSream.on('data', (data) => {
-                templateStr = data;
-                indexStr = indexStr.replace(templateName, templateStr);
-            });
-        }
-        const getIndexFile = fs.createWriteStream(indexFilePath, {encoding: "utf-8"});
-        getIndexFile.write(indexStr);
-    })
+    }
 }
 
 function mergeStyles(stylesCopyPath, mergedFilePath, stylesPath) {
